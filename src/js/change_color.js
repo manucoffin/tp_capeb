e82028d52d7fd6796a548da3be81ea564bbcd011
Manuel <manuel.coffin@gmail.com>
2016-12-05 18:25:34 +0100

    minor fixes

--- src/js/change_color.js
+++ src/js/change_color.js
@@ -2,27 +2,26 @@ window.addEventListener("load", function(){
     var divs = document.getElementsByClassName('color');
     
     for(var i=0; i<divs.length; i++){
-        divs[i].addEventListener("click", function(ev){
-            switch(ev.target.id) {
-                case 'black':
-                    document.getElementById("stylesheet").setAttribute("href", "black.css");
-                    console.log(ev.target.id);
-                    break;
-                case 'pink':
-                    document.getElementById("stylesheet").setAttribute("href", "pink.css");
-                    console.log(ev.target.id);
-                    break;
-                case 'yellow':
-                    document.getElementById("stylesheet").setAttribute("href", "yellow.css");
-                    console.log(ev.target.id);
-                    break;
-                case 'green':
-                    document.getElementById("stylesheet").setAttribute("href", "green.css");
-                    console.log(ev.target.id);
-                    break;
-                default:
-                    break;
-            }
-        })
+        divs[i].addEventListener("click", changeColor, false)
+        divs[i].addEventListener("touchstart", changeColor, false)
+    }
+    
+    function changeColor(ev){
+        switch(ev.target.id) {
+            case 'black':
+                document.getElementById("stylesheet").setAttribute("href", "black.css");
+                break;
+            case 'pink':
+                document.getElementById("stylesheet").setAttribute("href", "pink.css");
+                break;
+            case 'yellow':
+                document.getElementById("stylesheet").setAttribute("href", "yellow.css");
+                break;
+            case 'green':
+                document.getElementById("stylesheet").setAttribute("href", "green.css");
+                break;
+            default:
+                break;
+        }
     }
 })
